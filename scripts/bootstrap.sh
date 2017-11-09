49844400910fd7822e9e01cfe9490ce1a6c41424
Pierre Archambault <Pierre.Archambault@ca.ibm.com>
2017-11-09 18:39:57 -0500

    Added bash/cask shell completion

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -6,5 +6,11 @@ echo "Installing Homebrew: https://brew.sh"
 echo "Installing Homebrew Cask: https://caskroom.github.io"
 brew tap caskroom/cask
 
+echo "Brew install bash-completion"
+brew install bash-completion
+
+echo "Brew install cask/completion"
+brew install brew-cask-completion
+
 echo "Brew install git: https://git-scm.com/downloads"
 brew install git
