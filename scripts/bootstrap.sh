3edcaade024537fbaf72c19e83e9f978a64ec6ec
P/ <ve2caz@users.noreply.github.com>
2020-05-11 10:12:57 -0400

    Removed bash completion as it comes with brew
    Disabled more stuff as I work out what I really need.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -3,14 +3,11 @@
 echo "Installing Homebrew: https://brew.sh"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-echo "Installing Homebrew Cask: https://caskroom.github.io"
-brew tap caskroom/cask
+# echo "Installing Homebrew Cask: https://caskroom.github.io"
+# brew tap caskroom/cask
 
-echo "Brew install bash-completion"
-brew install bash-completion
-
-echo "Brew install cask/completion"
-brew install brew-cask-completion
+# echo "Brew install cask/completion"
+# brew install brew-cask-completion
 
 # echo "Brew install git: https://git-scm.com/downloads"
 # brew install git
