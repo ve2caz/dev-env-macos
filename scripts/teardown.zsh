f16037984dc07a36e24b97b65750976e4abfd726
P/ <ve2caz@users.noreply.github.com>
2021-12-02 17:49:05 -0500

    refactor(Cleanup): add utility functions + globals
    Cleanup and comenting out of cask removal as something is not working
    and needs further investigation

--- scripts/teardown.zsh
+++ scripts/teardown.zsh
@@ -1,29 +1,69 @@
-#!/bin/zsh -x
+#!/bin/zsh
 
 # Emulate BASH to make scripting easier
 emulate -LR bash
 
-# ASDF
-ASDF=~/.asdf
-rm -rf $ASDF
 
-# Homebrew Cask
-brew cask uninstall altair-graphql-client
-brew cask uninstall github
-brew cask uninstall graphql-playground
-brew cask uninstall intellij-idea-ce
-brew cask uninstall iterm2
-brew cask uninstall visual-studio-code
+####################
+# Helper Functions #
+####################
+function blankLine() {
+    echo
+}
 
-# Homebrew
-HOMEBREW_ROOT=~/homebrew
-rm -rf $HOMEBREW_ROOT
+function message() {
+    STAMP=`date +%Y-%m-%dT%H:%M:%S`
+    blankLine
+    echo "$STAMP --- $SCRIPT >>> $1"
+}
 
-# Oh My Zsh
+
+######################
+# Homebrew Functions #
+######################
+function caskUninstallIfNot() {
+    message "uninstall $1 if present"
+    blankLine
+    brew uninstall --cask $1
+}
+
+
+######################
+# GLOBAL DEFINITIONS #
+######################
+ASDF_ROOT=~/.asdf
+HOMEBREW_LOCAL_ROOT=~/homebrew
+OHMYZSH_ROOT=~/.oh-my-zsh
+OHMYZSH_CUSTOM_ROOT=$OHMYZSH_ROOT/custom
+OHMYZSH_CUSTOM_PLUGINS=$OHMYZSH_CUSTOM_ROOT/plugins
+OHMYZSH_CUSTOM_THEMES=$OHMYZSH_CUSTOM_ROOT/themes
+SCRIPT_PATH=${0:a:h}
+SCRIPT=${0:t}
+ZSH_USERS=https://github.com/zsh-users
 ZSHRC=~/.zshrc
 ZSHRC_BACKUP=~/.zshrc-dev-env-macos-backup
-OHMYZSH_ROOT=~/.oh-my-zsh
+
+
+# Homebrew Cask
+# caskUninstallIfNot altair-graphql-client
+# caskUninstallIfNot github
+# caskUninstallIfNot graphql-playground
+# caskUninstallIfNot intellij-idea-ce
+# caskUninstallIfNot iterm2
+# caskUninstallIfNot visual-studio-code
+
+
+# ASDF
+rm -rf $ASDF_ROOT
+
+
+# Homebrew
+rm -rf $HOMEBREW_LOCAL_ROOT
+
+
+# Oh My Zsh
 rm -rf $ZSHRC $ZSHRC_BACKUP $OHMYZSH_ROOT
+
 
 # Self
 echo "Please remove the dev-env-macos repo"
